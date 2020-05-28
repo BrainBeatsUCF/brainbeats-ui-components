10ab1a6e99c6b665d9ce74c390d721b6f04c1772
Lloyd Dapaah <lloyd.dapaah@yahoo.com>
2020-05-28 17:49:35 -0400

    Add css loader, add uimenubutton component, add project images

---
+++ src/Components/UIButton/UIButton.tsx
@@ -0,0 +1,51 @@
+import * as React from 'react';
+import { Component } from 'react';
+
+// let styles = require("./UIButton.css");
+import './UIButton.css';
+
+enum UIMenuButtonType {
+  Blue = "blue",
+  Red = "red"
+}
+
+interface IMenuButtonProps {
+  title: string,
+  image: string,
+  imageAlt: string,
+  imageHeight: number,
+  imageWidth: number,
+  buttonType: UIMenuButtonType,
+  onClick: () => void,
+  className?: string,
+  initAsActive?: boolean
+}
+
+class UIMenuButton extends Component<IMenuButtonProps, {}> {
+  constructor(props: IMenuButtonProps) {
+    super(props)
+  }
+
+  render() {
+    let overrideClassName = this.props.className ?? ''
+    let isActive = this.props.initAsActive ?? false
+    return (
+      <div
+        className={
+          `
+          NavigationInput 
+          ${isActive ? "NavigationInputActive" : ""} 
+          ${this.props.buttonType == UIMenuButtonType.Red ? "NavigationLogoutInput" : ""} 
+          ${overrideClassName}
+          `
+        }
+        onClick={() => this.props.onClick()}
+      >
+        <label>{this.props.title}</label>
+        <img src={this.props.image} alt={this.props.imageAlt} height={this.props.imageHeight} width={this.props.imageWidth}></img>
+      </div>
+    )
+  }
+}
+
+export { UIMenuButton, IMenuButtonProps, UIMenuButtonType }
--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,2 @@
 export * from './Components/SelectableButton';
+export * from './Components/UIButton/UIButton';
